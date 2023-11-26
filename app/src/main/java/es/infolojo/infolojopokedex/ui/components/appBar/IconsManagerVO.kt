8b243c2d2202b7d20cabad27c0e46523836dfcbd
ANTONIOJLO <ANTONIOJLO@ext.inditex.com>
2023-11-26 17:55:50 +0100

    Add AppBar main icon image and pokemons detail icon image

--- app/src/main/java/es/infolojo/infolojopokedex/ui/components/appBar/IconsManagerVO.kt
+++ app/src/main/java/es/infolojo/infolojopokedex/ui/components/appBar/IconsManagerVO.kt
@@ -2,6 +2,7 @@ package es.infolojo.infolojopokedex.ui.components.appBar
 
 /** VO to manage Icons and actions. */
 data class IconsManagerVO(
+    val appBarImage: String? = null,
     val showFilter: Boolean = false,
     val showPokemonNumber: Boolean = false
 )
